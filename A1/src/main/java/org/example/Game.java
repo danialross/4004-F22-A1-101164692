b33dd4e4b14e4f5ce300a58083226b9f1a424908
danialross <92746365+danialross@users.noreply.github.com>
2022-10-11 16:40:17 -0400

    <U-Test>  scorePoints func
    added scorePoints function with required enums and test

--- A1/src/main/java/org/example/Game.java
+++ A1/src/main/java/org/example/Game.java
@@ -1,6 +1,33 @@
 package org.example;
 
 
+import java.util.ArrayList;
+
 public class Game {
+
+    enum FortuneCard {
+        TREASURECHEST,
+        CAPTAIN,
+        SORCERESS,
+        SEABATTLE,
+        GOLD,
+        DIAMOND,
+        MONKEYBUSINESS,
+        SKULLS
+    }
+
+    enum Dice {
+        SKULL,
+        DIAMOND,
+        GOLD,
+        SWORD,
+        PARROT,
+        MONKEY
+    }
+   public int scorePoints(Dice[] playerHand, FortuneCard fc){
+
+        return 0;
+   }
+
 
 }
