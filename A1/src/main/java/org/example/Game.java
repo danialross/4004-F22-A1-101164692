8230f713d9c14c2421d9c52b52d1e79b089121ee
danialross <92746365+danialross@users.noreply.github.com>
2022-10-11 18:13:02 -0400

    <code> scorePoint function
    test passed for three of a kind dice role

--- A1/src/main/java/org/example/Game.java
+++ A1/src/main/java/org/example/Game.java
@@ -26,7 +26,35 @@ public class Game {
     }
    public int scorePoints(Dice[] playerHand, FortuneCard fc){
 
-        return 0;
+        int[] numOfRepeats = {0,0,0,0,0,0}; //{skull,diamond,gold,sword,parrot,monkey}
+        int score = 0;
+
+        for(int i = 0; i<playerHand.length; i++){
+            if(playerHand[i] == Dice.SKULL){
+                numOfRepeats[0]++;
+            }else if(playerHand[i] == Dice.DIAMOND){
+                numOfRepeats[1]++;
+            }else if(playerHand[i] == Dice.GOLD){
+                numOfRepeats[2]++;
+            }else if(playerHand[i] == Dice.SWORD){
+                numOfRepeats[3]++;
+            }else if(playerHand[i] == Dice.PARROT){
+                numOfRepeats[4]++;
+            }else{ // Monkey
+                numOfRepeats[5]++;
+            }
+        }
+
+        for(int j = 0; j<numOfRepeats.length; j++){
+            if(j==3){
+                score += 100;
+            }
+        }
+
+
+
+
+        return score;
    }
 
 
