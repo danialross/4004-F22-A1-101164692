c15397142099c9bb1506e86048877dcdee8bc1ac
danialross <92746365+danialross@users.noreply.github.com>
2022-10-11 21:21:10 -0400

    <U-test> scorepoints
    added test for diamonds from 1 to 8 with 3 of a kinds to 8 of a kind with full chest bonus

--- A1/src/test/java/org/example/GameTest.java
+++ A1/src/test/java/org/example/GameTest.java
@@ -33,6 +33,33 @@ class GameTest {
         // eight of a kind and fullChest bonus
         assertEquals(4500, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
 
+        // 1 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.PARROT, Game.Dice.PARROT, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.MONKEY};
+        assertEquals(100, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 2 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.PARROT, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.MONKEY};
+        assertEquals(200, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 3 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.MONKEY};
+        assertEquals(400, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 4 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.SWORD, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.MONKEY};
+        assertEquals(600, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 5 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.MONKEY};
+        assertEquals(1000, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 6 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.SKULL, Game.Dice.MONKEY};
+        assertEquals(1600, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 7 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.MONKEY};
+        assertEquals(2700, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+        // 8 diamond
+        hand = new Game.Dice[]{Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.DIAMOND};
+        assertEquals(5300, game.scorePoints(hand,Game.FortuneCard.TREASURECHEST));
+
+
+
 
 
 
