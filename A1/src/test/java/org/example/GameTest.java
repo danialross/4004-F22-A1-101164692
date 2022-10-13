5854c501dce40e7f7ddee04892f78ff67126d402
danialross <92746365+danialross@users.noreply.github.com>
2022-10-13 17:07:20 -0400

    <U-Test> scorePoints
    add test for both 1xSkull and 2xSkull

--- A1/src/test/java/org/example/GameTest.java
+++ A1/src/test/java/org/example/GameTest.java
@@ -112,6 +112,15 @@ class GameTest {
         hand = new Game.Dice[]{Game.Dice.GOLD, Game.Dice.GOLD, Game.Dice.GOLD, Game.Dice.GOLD, Game.Dice.GOLD, Game.Dice.GOLD, Game.Dice.GOLD, Game.Dice.GOLD};
         assertEquals(10600, game.scorePoints(hand,Game.FortuneCard.CAPTAIN));
 
+        //skulls fortune card
+        hand = new Game.Dice[]{Game.Dice.MONKEY, Game.Dice.MONKEY, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.MONKEY, Game.Dice.MONKEY};
+        assertEquals(0, game.scorePoints(hand,Game.FortuneCard.SKULLX1));
+        hand = new Game.Dice[]{Game.Dice.MONKEY, Game.Dice.GOLD, Game.Dice.SKULL, Game.Dice.DIAMOND, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.MONKEY, Game.Dice.MONKEY};
+        assertEquals(300, game.scorePoints(hand,Game.FortuneCard.SKULLX1));
+        hand = new Game.Dice[]{Game.Dice.MONKEY, Game.Dice.DIAMOND, Game.Dice.SKULL, Game.Dice.GOLD, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.MONKEY, Game.Dice.MONKEY};
+        assertEquals(0, game.scorePoints(hand,Game.FortuneCard.SKULLX2));
+        hand = new Game.Dice[]{Game.Dice.MONKEY, Game.Dice.DIAMOND, Game.Dice.DIAMOND, Game.Dice.GOLD, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.MONKEY, Game.Dice.MONKEY};
+        assertEquals(300, game.scorePoints(hand,Game.FortuneCard.SKULLX2));
 
 
 
