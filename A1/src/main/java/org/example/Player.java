bfbec1212da919d1fb498cfcec87d64434384a4d
danialross <92746365+danialross@users.noreply.github.com>
2022-10-18 20:16:08 -0400

    added function for base round
    base round function when a normal round is played, has skeleton for the type of input accepted by the user.

--- A1/src/main/java/org/example/Player.java
+++ A1/src/main/java/org/example/Player.java
@@ -30,6 +30,51 @@ public class Player {
         System.out.println("---- Fortune Card : " + fc + " ----");
     }
 
+    public int playBaseRound(){
+        Game.Dice[] emptyArr = {};
+
+        game.printStatus("start");
+        Scanner scanner = new Scanner(System.in);
+        boolean stop = false;
+
+        while(stop == false){
+            System.out.println("Select an action:");
+            System.out.println("1. Choose dice to roll again");
+            System.out.println("2. Reroll all dice");
+            System.out.println("3. Score with current hand");
+
+            if(fc == Game.FortuneCard.SORCERESS && game.isContainSkull(playerRoll)){
+                System.out.println("4. Reroll a skull");
+
+            }else if(fc == Game.FortuneCard.TREASURECHEST){
+                System.out.println("4. Save Dice in treasure chest");
+
+                if(savedDice.length != 0 ){
+                    System.out.println("5. Take dice out from treasure chest");
+                }
+            }
+
+            int action = scanner.nextInt();
+
+            if(action == 1){
+
+            }else if(action == 2){
+
+
+            }else if(action == 3){
+
+
+            }else if(action == 4){
+
+
+            }else if(action == 5){
+                
+            }
+
+        }
+        game.printStatus("end");
+        return score;
+    }
 
 
 }
