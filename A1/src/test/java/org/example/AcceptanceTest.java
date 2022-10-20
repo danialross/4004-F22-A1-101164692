9195255913d81597ccba958c3c3e83e6295b5021
danialross <92746365+danialross@users.noreply.github.com>
2022-10-20 11:48:59 -0400

    <A-Test>
    added test for row47 and passed

--- A1/src/test/java/org/example/AcceptanceTest.java
+++ A1/src/test/java/org/example/AcceptanceTest.java
@@ -47,9 +47,37 @@ class AcceptanceTest {
 
         p1.setPlayerRoll(game.rollDice(p1.getPlayerRoll(),riggedhand));
         p1.setPlayerRoll(game.changeNullToDice(p1.getPlayerRoll(),p1.getSavedDicePos(),p1.getSavedDice()));
-        
+
         p1.checkDead();
 
         assertEquals(0,p1.getScore());
+    }
+
+    @Test
+    void row47(){
+        //roll 2 skulls, 4 parrots, 2 swords, reroll swords, get 1 skull 1 sword  die
+        Player p1 = new Player("p1");
+        Game game = p1.game;
+
+        Game.Dice[] playerHand = {};
+        Game.Dice[] riggedhand = {Game.Dice.SKULL,Game.Dice.SKULL,Game.Dice.PARROT,Game.Dice.PARROT,Game.Dice.PARROT,Game.Dice.PARROT,Game.Dice.SWORD,Game.Dice.SWORD};
+
+        p1.setPlayerRoll(game.rollDice(playerHand,riggedhand));
+
+        int[] userInput = {0,1,2,3,4,5};
+        p1.setSavedDicePos(userInput);
+        p1.setSavedDice(game.getSavedDice(p1.getPlayerRoll(),userInput));
+
+        p1.setPlayerRoll(game.changeDiceToNull(p1.getPlayerRoll(),p1.getSavedDicePos()));
+        riggedhand = new Game.Dice[]{Game.Dice.SKULL,Game.Dice.SKULL,Game.Dice.PARROT,Game.Dice.PARROT,Game.Dice.PARROT,Game.Dice.PARROT,Game.Dice.SKULL,Game.Dice.SWORD};
+
+        p1.setPlayerRoll(game.rollDice(p1.getPlayerRoll(),riggedhand));
+        p1.setPlayerRoll(game.changeNullToDice(p1.getPlayerRoll(),p1.getSavedDicePos(),p1.getSavedDice()));
+
+        p1.checkDead();
+
+        assertEquals(0,p1.getScore());
+
+
     }
 }
