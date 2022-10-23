f070c73d13dfc7f024f39921708212a16f8603bd
danialross <92746365+danialross@users.noreply.github.com>
2022-10-23 14:41:20 -0400

    <U-Test>
    added test for IOD

--- A1/src/test/java/org/example/PlayerTest.java
+++ A1/src/test/java/org/example/PlayerTest.java
@@ -177,6 +177,10 @@ class PlayerTest {
         result = p1.promptUI(null);
         assertEquals(-1,result);
 
+        p1.setPlayerRoll(new Game.Dice[]{Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.SKULL, Game.Dice.PARROT, Game.Dice.PARROT, Game.Dice.PARROT, Game.Dice.PARROT});
+        result = p1.promptUI(null);
+        assertEquals(-2,result);
+
 
     }
 
