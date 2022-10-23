bc3b9c490e67d0462917628fd35826c281777466
danialross <92746365+danialross@users.noreply.github.com>
2022-10-23 16:58:39 -0400

    <refactor>
    row 54

--- A1/src/test/java/org/example/AcceptanceTest.java
+++ A1/src/test/java/org/example/AcceptanceTest.java
@@ -157,23 +157,19 @@ class AcceptanceTest {
 
     @Test
     void row54(){
+        Game.Dice[] riggedhand = {Game.Dice.MONKEY, Game.Dice.MONKEY, Game.Dice.MONKEY, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SKULL, Game.Dice.SKULL};
+        Game.FortuneCard riggCard =  Game.FortuneCard.GOLD;
         //roll 3 (monkey, swords) + 2 skulls and score   (SC 300)
         Player p1 = new Player("p1");
-        Game game = p1.game;
+        p1.roundStarting();
 
-        Game.Dice[] playerHand = {};
-        Game.Dice[] riggedhand = {Game.Dice.MONKEY, Game.Dice.MONKEY, Game.Dice.MONKEY, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SWORD, Game.Dice.SKULL, Game.Dice.SKULL};
+        p1.setPlayerRoll(riggedhand);
+        p1.setFc(riggCard);
 
-        p1.setPlayerRoll(game.rollDice(playerHand, riggedhand));
-        p1.setFc(game.pickCard(Game.FortuneCard.GOLD));
-
-        p1.setPlayerRoll(game.rollDice(p1.getPlayerRoll(), riggedhand));
-
-        p1.checkDead();
-        p1.setScore(game.scorePoints(p1.getPlayerRoll(), p1.getFc()));
+        p1.promptUI("3");
+        p1.doOption(3,new Game.Dice[]{},null, null);
 
         assertEquals(300, p1.getScore());
-
     }
 
     @Test
